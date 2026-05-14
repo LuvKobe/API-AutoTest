9db3ce8933a8f9ab33fb05c98c6fd91f274f5bdb
LuvKobe <3078601436@qq.com>
2026-05-14 16:19:00 +0800

    add: 常见请求方法代码示例

--- apiAutoTest/test01.py
+++ apiAutoTest/test01.py
@@ -7,10 +7,16 @@ import requests
 
 get_r = requests.get("https://www.baidu.com")
 post_r = requests.post("https://www.baidu.com")
-req_r1 = requests.request(method="get", url="https://www.baidu.com")
-req_r2 = requests.request(method="post", url="https://www.baidu.com")
+
+#req_r1 = requests.request(method="get", url="https://www.baidu.com")
+#req_r2 = requests.request(method="post", url="https://www.baidu.com")
+
+req_get = requests.request("GET", "https://www.baidu.com")
+req_post = requests.request("POST", "https://www.baidu.com")
 
 print("get:", get_r.status_code)
 print("post:", post_r.status_code)
-print("method_get:", req_r1.status_code)
-print("method_post:", req_r2.status_code)
+# print("method_get:", req_r1.status_code)
+# print("method_post:", req_r2.status_code)
+print("method_get:", req_get)
+print("method_post:", req_post)
