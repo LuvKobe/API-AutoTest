05b50c44a479bbdbfff78de8c2602b827c7830c2
LuvKobe <3078601436@qq.com>
2026-05-14 15:53:24 +0800

    add: 常见请求方法代码示例

---
+++ apiAutoTest/test01.py
@@ -0,0 +1,16 @@
+import requests
+
+# r = requests.get("https://www.baidu.com")
+#
+# print(r.status_code)
+# print(r.text)
+
+get_r = requests.get("https://www.baidu.com")
+post_r = requests.post("https://www.baidu.com")
+req_r1 = requests.request(method="get", url="https://www.baidu.com")
+req_r2 = requests.request(method="post", url="https://www.baidu.com")
+
+print("get:", get_r.status_code)
+print("post:", post_r.status_code)
+print("method_get:", req_r1.status_code)
+print("method_post:", req_r2.status_code)
