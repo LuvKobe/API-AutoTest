a5b3297b3abbc26aa290e2dc44452f186c9a9225
LuvKobe <3078601436@qq.com>
2026-05-14 17:39:33 +0800

    add: pytest 测试框架

--- apiAutoTest/test_02.py
+++ apiAutoTest/test_02.py
@@ -1,17 +1,23 @@
 import requests
 
-class Test():
-    def test01(self):
-        r = requests.get("http://www.baidu.com")
-        print(r.text)
+# class Test():
+#     def test01(self):
+#         r = requests.get("http://www.baidu.com")
+#         print(r.text)
+#
+#     def test02(self):
+#         url = "http://121.196.200.210:8080/user/login"
+#
+#         data = {
+#             "userName": "zhangsan",
+#             "password": "123456"
+#         }
+#
+#         r = requests.request(method="POST",url=url, data=data)
+#         print(r.json())
 
-    def test02(self):
-        url = "http://121.196.200.210:8080/user/login"
-
-        data = {
-            "userName": "zhangsan",
-            "password": "123456"
-        }
-
-        r = requests.request(method="POST",url=url, data=data)
-        print(r.json())
+# class Test():
+#     def __init__(self):
+#         print("-----init-------")
+#     def test_a(self):
+#         print("-----test_a----")
