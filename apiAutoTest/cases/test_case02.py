3071b7aaa20675468a6bc2889036e0db3e051d1f
LuvKobe <3078601436@qq.com>
2026-05-15 16:56:06 +0800

    add: setup_class 和 teardown_class

--- apiAutoTest/cases/test_case02.py
+++ apiAutoTest/cases/test_case02.py
@@ -1,6 +1,19 @@
+# class Test:
+#     def setup_method(self):
+#         print("Setup: Before each test")
+#
+#     def test01(self):
+#         print("test01方法")
+#
+#     def test02(self):
+#         print("test02方法")
+#
+#     def teardown_method(self):
+#         print("Teardown: After each test")
+
 class Test:
-    def setup_method(self):
-        print("Setup: Before each test")
+    def setup_class(self):
+        print("Setup: Before all test")
 
     def test01(self):
         print("test01方法")
@@ -8,5 +21,5 @@ class Test:
     def test02(self):
         print("test02方法")
 
-    def teardown_method(self):
-        print("Teardown: After each test")
+    def teardown_class(self):
+        print("Teardown: After all test")
